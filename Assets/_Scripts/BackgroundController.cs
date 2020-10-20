66fff569592884d0d18e4cff503fd1557cb6cd82
Teurowel <teurowel@gmail.com>
2020-10-20 23:32:26 +0900

    Changed background movement to horizontal

--- Assets/_Scripts/BackgroundController.cs
+++ Assets/_Scripts/BackgroundController.cs
@@ -1,11 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//Source file name : BackgroundController.cs
+//Student name : Doosung Jang
+//Student ID : 101175013
+//Date last modified : October, 20. 2020
+//Program description : This class will control background movement
+//Revision history : October, 20. 2020 : Added horizontalSpeed, horizontalBoundary varibales
+//                                       Changed _Move, _Reset, _CheckBounds to adapt horizontal movement
+
+
+//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    public float horizontalSpeed;
+    public float horizontalBoundary;
 
     // Update is called once per frame
     void Update()
@@ -16,18 +25,18 @@ public class BackgroundController : MonoBehaviour
 
     private void _Reset()
     {
-        transform.position = new Vector3(0.0f, verticalBoundary);
+        transform.position = new Vector3(horizontalBoundary, 0.0f);
     }
 
     private void _Move()
     {
-        transform.position -= new Vector3(0.0f, verticalSpeed) * Time.deltaTime;
+        transform.position -= new Vector3(horizontalSpeed, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
         // if the background is lower than the bottom of the screen then reset
-        if (transform.position.y <= -verticalBoundary)
+        if (transform.position.x <= -horizontalBoundary)
         {
             _Reset();
         }
